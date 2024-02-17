28f8dc6cadea5748f9805bd29418a124723fe534
Maciej Kwapisz <ciamek.77@gmail.com>
2024-02-17 23:47:15 +0100

    Change en/de subcommands to encrypt.decrypt

--- main.go
+++ main.go
@@ -9,28 +9,28 @@ import (
 )
 
 func main() {
-	encryptCmd := flag.NewFlagSet("en", flag.ExitOnError)
+	encryptCmd := flag.NewFlagSet("encrypt", flag.ExitOnError)
 	encryptFile := encryptCmd.String("f", "", "File to encrypt (required)")
 	encryptKey := encryptCmd.String("k", "", "Key used for encryption (optional)")
 
-	decryptCmd := flag.NewFlagSet("de", flag.ExitOnError)
+	decryptCmd := flag.NewFlagSet("decrypt", flag.ExitOnError)
 	decryptFile := decryptCmd.String("f", "", "File to decrypt (required)")
 	decryptKey := decryptCmd.String("k", "", "Key used for decryption (required)")
 
 	if len(os.Args) < 2 {
-		fmt.Println("expected 'en' or 'de' subcommands")
+		fmt.Println("expected 'encrypt' or 'decrypt' subcommands")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
-	case "en":
+	case "encrypt":
 		encryptCmd.Parse(os.Args[2:])
 		err := handlers.EncryptFile(*encryptFile, *encryptKey)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
-	case "de":
+	case "decrypt":
 		decryptCmd.Parse(os.Args[2:])
 		err := handlers.DecryptFile(*decryptFile, *decryptKey)
 		if err != nil {
@@ -38,7 +38,7 @@ func main() {
 			os.Exit(1)
 		}
 	default:
-		fmt.Println("expected 'en' or 'de' subcommands")
+		fmt.Println("expected 'encrypt' or 'decrypt' subcommands")
 		os.Exit(1)
 	}
 }
